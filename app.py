5dfa509c3a96ab643c71d6b906989627fe5d073e
Himanshu Singh <121339915+HimanshuGitCode@users.noreply.github.com>
2024-05-24 11:08:27 +0530

    app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os 
 import streamlit as st
 from langchain_groq import ChatGroq
-from langchain.text_splitter import RecursiveTextSplitter
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate
 from langchain.chains import create_retrieval_chain
@@ -26,4 +26,35 @@ prompt = ChatPromptTemplate.from_template(""" Answer the questions based on the 
 
 def vector_embedding(): 
     
-    if "vector" not in st.session_state
+    if "vectors" not in st.session_state:
+        st.session_state.embeddings=GoogleGenerativeAIEmbeddings(model ="models/embedding-001")
+        st.session_state.loader=PyPDFDirectoryLoader("./us_census") #data ingestion step
+        st.session_state.docs=st.session_state.loader.load() #document loadings
+        st.session_state.text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200) #text splitter
+        st.session_state.final_documents=st.session_state.text_splitter.split_documents(st.session_state.docs) #splitting the documents
+        st.session_state.vector_store = FAISS.from_documents(st.session_state.final_documents, st.session_state.embeddings) #vector store
+        
+prompt1=st.text_input("What you want to ask form the document?")
+
+if st.button("Creating Vector Store"):
+    vector_embedding()
+    st.write("Vector Stor DB is Ready")
+    
+import time 
+
+if prompt1:
+    document_chain = create_stuff_documents_chain(llm,prompt)
+    retriever=st.session_state.vectors.as_retriever()  
+    retrieval_chain = create_retrieval_chain(retriever, document_chain)
+    
+    start = time.process_time()
+    response = retrieval_chain.invoke({'input':prompt1})
+    st.write(response['answer'])
+    
+    #with a streamlit expander
+    with st.expander("Document Similartiy Search"):
+        #find the relevant chunks
+        for i , doc in enumerate(response["context"]):
+            st.write(doc.page_content)
+            st.write("---------------------------------")      
+        
